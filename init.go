2068f9b7f37cf197296354efd5247bd1b2920ad9
gzleo <yatseni3@gmail.com>
2016-03-29 17:36:24 +0800

    split zend and phpgo package.

--- init.go
+++ init.go
@@ -1,10 +1,10 @@
 package zend
 
 /*
-#include "extension.h"
-*/
+ */
 import "C"
-import "unsafe"
+
+// import "unsafe"
 import "runtime"
 
 import "time"
@@ -14,21 +14,6 @@ import "math/rand"
 import "fmt"
 
 //
-// 给php加载扩展时调用的扩展入口函数。
-//export get_module
-func get_module() unsafe.Pointer {
-	if len(ExtName) == 0 {
-		panic("ext name not set.")
-	}
-
-	addBuiltins()
-
-	mod := C.phpgo_get_module(C.CString(ExtName), C.CString(ExtVer))
-	fmt.Printf("mod=%p\n", mod)
-
-	return unsafe.Pointer(mod)
-}
-
 func init() {
 	omp := runtime.GOMAXPROCS(1)
 	fmt.Println("run gozend init...", omp)
