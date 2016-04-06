5362b4a832260a72437944a1e331c6dd5366da86
gzleo <yatseni3@gmail.com>
2016-04-06 17:17:59 +0800

    fix php7 support

--- szend.c
+++ szend.c
@@ -9,16 +9,12 @@
 #endif
 #include "zend_API.h"
 
-
-int gozend_call_user_function(zval **object, char *func_name, zval *retval_ptr, int argc, zval **params)
+#ifdef ZEND_ENGINE_3
+int gozend_call_user_function(zval *object, char *func_name, zval *retval_ptr, int argc, zval *params)
 {
     zval function_name;
-    INIT_ZVAL(function_name);
-#ifdef ZEND_ENGINE_3
+    ZVAL_NULL(&function_name);
     ZVAL_STRING(&function_name, func_name);
-#else
-    ZVAL_STRING(&function_name, func_name, 1);
-#endif
 
     assert(retval_ptr != NULL);
     if (call_user_function(CG(function_table), object , &function_name,
@@ -27,33 +23,70 @@ int gozend_call_user_function(zval **object, char *func_name, zval *retval_ptr, 
     }
     return -1;
 }
-
-int gozend_call_user_function_string_ex(char *func_name, char *str, char **retstr, zval **object)
+#else /* ZEND_ENGINE_2 */
+int gozend_call_user_function(zval **object, char *func_name, zval *retval_ptr, int argc, zval **params)
 {
-    zval *args[9];
-    MAKE_STD_ZVAL(args[0]);
-#ifdef ZEND_ENGINE_3
-    ZVAL_STRING(args[0], str);
-#else
-    ZVAL_STRING(args[0], str, 1);
+  zval function_name;
+  INIT_ZVAL(function_name);
+  ZVAL_STRING(&function_name, func_name, 1);
+
+  assert(retval_ptr != NULL);
+  if (call_user_function(CG(function_table), object , &function_name,
+                         retval_ptr, argc, params TSRMLS_CC) == SUCCESS) {
+    return 0;
+  }
+  return -1;
+}
 #endif
 
-    zval retval_ptr;
-    INIT_ZVAL(retval_ptr);
+
+#ifdef ZEND_ENGINE_3
+static int gozend_call_user_function_string_ex(char *func_name, char *str, char **retstr, zval *object)
+{
+  zval args[9];
+  ZVAL_NULL(&args[0]);
+  ZVAL_STRING(&args[0], str);
+
+  zval retval_ptr;
+  ZVAL_NULL(&retval_ptr);
 
     int ret = gozend_call_user_function(object, func_name, &retval_ptr, 1, args);
     if (ret == 0) {
-        char *cstr = NULL;
-        int cslen = 0;
+    char *cstr = NULL;
+    int cslen = 0;
 
-        if (Z_TYPE_P(&retval_ptr) == IS_STRING) {
-            cslen = Z_STRLEN_P(&retval_ptr);
-            cstr = estrndup(Z_STRVAL_P(&retval_ptr), cslen);
-            *retstr = cstr;
-        }
+    if (Z_TYPE_P(&retval_ptr) == IS_STRING) {
+      cslen = Z_STRLEN_P(&retval_ptr);
+      cstr = estrndup(Z_STRVAL_P(&retval_ptr), cslen);
+      *retstr = cstr;
     }
-    return ret;
+  }
+  return ret;
 }
+#else /* ZEND_ENGINE_2 */
+static int gozend_call_user_function_string_ex(char *func_name, char *str, char **retstr, zval **object)
+{
+  zval *args[9];
+  MAKE_STD_ZVAL(args[0]);
+  ZVAL_STRING(args[0], str, 1);
+
+  zval retval_ptr;
+  ZVAL_NULL(&retval_ptr)
+
+    int ret = gozend_call_user_function(object, func_name, &retval_ptr, 1, args);
+  if (ret == 0) {
+    char *cstr = NULL;
+    int cslen = 0;
+
+    if (Z_TYPE_P(&retval_ptr) == IS_STRING) {
+      cslen = Z_STRLEN_P(&retval_ptr);
+      cstr = estrndup(Z_STRVAL_P(&retval_ptr), cslen);
+      *retstr = cstr;
+    }
+  }
+  return ret;
+}
+#endif
 
 
 char *gozend_call_user_function_string(char *func_name, char *str)
@@ -73,9 +106,9 @@ int gozend_iszts() {
 
 int gozend_zend_version_no() {
 #ifdef ZEND_ENGINE_3
-     return ZEND_ENGINE_3;
+     return 3;
 #else
-     return ZEND_ENGINE_2;
+     return 2;
 #endif
 }
 
@@ -110,7 +143,11 @@ void *gozend_erealloc(void *ptr, size_t size) {
 char gozend_eval_string(char *code)
 {
     zval retval;
+    #ifdef ZEND_ENGINE_3
+    ZVAL_NULL(&retval);
+    #else
     INIT_ZVAL(retval);
+    #endif
 
     int ret = zend_eval_string(code, &retval, (char*)"" TSRMLS_CC);
 
@@ -120,27 +157,19 @@ char gozend_eval_string(char *code)
 }
 
 
-
 void call_user_function_callback(char *data)
 {
-    zval *args[9];
-    MAKE_STD_ZVAL(args[0]);
-#ifdef ZEND_ENGINE_3
-    ZVAL_STRING(args[0], data);
-#else
-    ZVAL_STRING(args[0], data, 1);
-#endif
     uint32_t argc = 0;
-
     zval retval_ptr;
-    INIT_ZVAL(retval_ptr);
-
     char *func_name = "say_hello_123";
     zval function_name;
-    INIT_ZVAL(function_name);
 #ifdef ZEND_ENGINE_3
+    ZVAL_NULL(&retval_ptr);
+    ZVAL_NULL(&function_name);
     ZVAL_STRING(&function_name, func_name);
 #else
+    INIT_ZVAL(retval_ptr);
+    INIT_ZVAL(function_name);
     ZVAL_STRING(&function_name, func_name, 1);
 #endif
 
