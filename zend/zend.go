1e977f61756c26a445974eab64ffbd77fd248f33
kitech <kitech@users.sf.net>
2016-06-02 14:36:41 +0800

    fix OS X ar support.

--- zend/zend.go
+++ zend/zend.go
@@ -12,7 +12,8 @@ import (
 // #cgo LDFLAGS: -L/home/dev/php5/lib -lphp5
 #cgo CFLAGS: -g -O2 -std=c99 -D_GNU_SOURCE
 // #cgo LDFLAGS: -lphp5
-#cgo LDFLAGS: -Wl,--warn-unresolved-symbols -Wl,--unresolved-symbols=ignore-all
+#cgo linux LDFLAGS: -Wl,--warn-unresolved-symbols -Wl,--unresolved-symbols=ignore-all
+#cgo darwin LDFLAGS: -undefined dynamic_lookup
 
 #include <stdlib.h>
 #include "sztypes.h"
